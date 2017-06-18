7bc20b7f9735b220dcc18ef96ecf944c44123635
CJ Sampson <cjsampson41@gmail.com>
2017-06-18 13:48:37 -0600

    reducer stopwatch fix

--- src/reducers/stopwatch.js
+++ src/reducers/stopwatch.js
@@ -5,15 +5,16 @@ const initialState = {
 
 
 const stopwatch = ( state = initialState, action) => {
+  console.log('i am from stopWatch reducer')
   switch (action.type) {
     case 'START_TIMER':
       return Object.assign({}, state);
     case 'TIMER_TICK':
-      // return Object.assign({}, state, { elapsed: 60, startTime: startTime--})
-    // return {...state, elapsed: state.startTime - 1, startTime: state.startTime --}
-     // return Object.assign({}, state, { elapsed: state.startTime - 1 });
-    // }
-    // }
+      return { 
+        ...state, 
+        elapsed: state.startTime - 1, 
+        startTime: ( state.startTime - 1)
+      }
     case 'STOP_TIMER':
       return state;
     default:
